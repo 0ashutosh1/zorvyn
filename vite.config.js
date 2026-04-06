09a718d781a9622826bbff4f91dade21a1706e6d
Kumar Ashutosh <ashutosh@Kumars-MacBook-Air.local>
2026-04-06 11:39:52 +0530

    Support Render root base path

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+const isRender = Boolean(process.env.RENDER)
+
 export default defineConfig({
-  base: '/zorvyn/',
+  base: isRender ? '/' : '/zorvyn/',
   plugins: [react()],
   esbuild: {
     jsx: 'automatic',
